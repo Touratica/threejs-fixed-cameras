046485f8e96a441cbcc01bc3b37c3fb246d2903a
JoaninhaRaposo <joana.raposo@tecnico.ulisboa.pt>
2020-10-01 18:08:11 +0100

    esboço do codigo com classes

---
+++ src/js/element.js
@@ -0,0 +1,43 @@
+'use strict';
+class Element extends THREE.Object3D {
+
+    material;
+    mesh;
+
+    constructor(x, y, z) {
+        super();
+        this.create(this, x, y, z);
+    }
+
+    addParallelepipedVertical(obj, x, y, z, w, h, d) {
+        var geometry = new THREE.CubeGeometry(w, h, d);
+        geometry.rotateX(Math.PI/2); /*verificar estas contas...*/ 
+        obj.mesh = new THREE.Mesh(geometry,obj.material);
+        obj.mesh.position.set(x, y, z);
+        obj.add(obj.mesh);
+    }
+
+    addCylinderVertical(obj,x,y,z,base,height){
+        var geometry = new THREE.CylinderGeometry( base, base, height, 64 ); /*Verificar estes parametros*/
+        geometry.rotateX(Math.PI/2);
+        obj.mesh = new THREE.Mesh(geometry,obj.material);
+        obj.mesh.position.set(x,y,z);
+        obj.add(mesh);
+    
+    }
+    
+    addCylinderHorizontal(obj,x,y,z,base,height){
+    
+        var geometry = new THREE.CylinderGeometry( base, base, height, 64 );
+        obj.mesh = new THREE.Mesh(geometry,material); /*funciona msm assim ou sera que precisa de rot?*/
+        obj.mesh.position.set(x,y,z);
+        obj.add(mesh);
+    }
+    /*Fiquei por aqui*/ 
+
+    changeWireframe() {
+        this.material.wireframe = !this.material.wireframe;
+    }
+
+    create() {}
+}
