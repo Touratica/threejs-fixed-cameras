fbdcff84b975a34261f8d32eed151c37abb75aa4
JoaninhaRaposo <joana.raposo@tecnico.ulisboa.pt>
2020-10-01 18:06:04 +0100

    esboço do codigo com divisao em classes

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 /*global THREE*/
 
-let renderer, scene, camera;
+var camera = [] , currentCamera =0;
+var scene, renderer;
 
-let distance = 10;
+//let distance = 5;
 
-function addRod(obj, x, y, z, h, a) {
+/*function addRod(obj, x, y, z, h, a) {
 	'use strict';
 
 	let geometry = new THREE.CylinderGeometry(1, 1, h);
@@ -25,35 +26,59 @@ function createMobile(x, y, z) {
 
 	scene.add(mobile);
 
-}
+}*/
 
-function createCamera() {
+function createCamera(x,y,z) {
 	'use strict';
-	camera = new THREE.OrthographicCamera(-8 * distance, 8 * distance,
+	/*camera = new THREE.OrthographicCamera(-8 * distance, 8 * distance,
 		4.5 * distance,-4.5 * distance);
-	scene.add(camera);
+	scene.add(camera);*/
+	var distance = 5;
+
+   	camera = new THREE.OrthographicCamera( window.innerWidth / -distance, 
+                                            window.innerWidth / distance, 
+                                            window.innerHeight / distance, 
+                                            window.innerHeight / -distance, 
+                                            1, 
+											1000);
+	camera.position.x = x;
+    camera.position.y = y;
+    camera.position.z = z;
+	camera.lookAt(scene.position);
+	return camera;
+
 }
 
 function createScene() {
 	'use strict';
 	scene = new THREE.Scene();
 
-	scene.add(new THREE.AxesHelper(10));
+	scene.add(new THREE.AxesHelper(20));
 
-	createMobile(0,0,0);
+	//createMobile(0,0,0); /*Acho melhor criar uma classe chamada Mobile, dps faz-se new Mobile*/
+	
+
 }
 
 function animate() {
 	'use strict';
+
+	/*funcoes de animacao sobre o mobile*/
 
 	render();
 
 	requestAnimationFrame(animate);
 }
 
-function render() {
+/*function render() {
 	'use strict';
 	renderer.render(scene, camera);
+	
+}*/
+
+function render() {
+	'use strict';
+    renderer.render(scene, camera[currentCamera]);
 }
 
 function __init__() {
@@ -66,5 +91,82 @@ function __init__() {
 	document.body.appendChild(renderer.domElement);
 
 	createScene();
-	createCamera();
+	camera[0]= createCamera(100,0,0); //view from x
+	camera[1]=createCamera(0,0,100); //view from z
+	camera[2]= createCamera(0,100,0); //view from y
+	render();
+	window.addEventListener("keydown",onKeyDown);
 }
+
+function onKeyDown(e) {   
+    switch (e.keyCode) {
+
+        case 49: //1
+            currentCamera = 0;
+            break;
+
+        case 50: //2
+            currentCamera = 1;
+            break;
+
+        case 51:  //3
+            currentCamera = 2;
+            break;
+
+        case 52: //4
+			/* Fazer algo tipo mobile.changeWireframe();*/	
+	
+			break;
+			
+		case 87: // W
+		case 119: // w   /*Rodar v1 num sentido*/
+			/*Ter algo do tipo mobile.rotate(v1,Right)*/
+			break;
+	
+		case 81: // Q  /*rodar v1 noutro sentido*/
+		case 113: //q
+		/*mobile.rotate(v1,left)*/
+				
+		break;      
+	
+		case 65: // A
+        case 97: //a
+           	/* mobile.rotate(v2,right)*/
+			break;
+			
+		case 68:  //D /* Procurar d minusculo*/
+		/*mobile.rotate(v2,left)*/  /*Acho que ramo1 ramo 2 e ramo 3 devem ser partes do mobile separadas,
+		depois podemos fazer ramo1.rotate(right), por exemplo.*/
+		case 90:  //Z /* procurar z minusculo*/
+		/*v3.rotate(right);*/
+			break;
+
+        case 83: // S
+        case 115: //s
+            /*v3.rotate(left)*/
+			break;
+			case 38: // Seta para cima
+        	/*mobile.move()*/
+
+            break;
+
+        case 40: // Seta para baixo
+            /*mobile.move()*/
+            break;
+
+        case 39: // Seta para o lado direito
+            /*mobile.move()*/
+            break;
+        
+        case 37: // Seta para o lado esquerdo
+           /*mobile.move()*/
+            break;
+
+       
+    }
+
+}
+
+
+
+
