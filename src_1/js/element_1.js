4cf4c801a006483b39d72d76d946c10482425bb1
Leonor Santos <57658771+leonortsantos@users.noreply.github.com>
2020-10-05 19:44:41 +0100

    versao 2 das classes

---
+++ src_1/js/element_1.js
@@ -0,0 +1,39 @@
+'use strict';
+class Elemento extends THREE.Object3D {
+
+    addParallelepipedVertical(material,x, y, z, w, h) {
+        var geometry = new THREE.BoxGeometry(w, h, w);
+        var mesh= new THREE.Mesh(geometry,material);
+        this.add(mesh);
+        mesh.rotateX(Math.PI/2);
+        mesh.position.set(x, y, z);
+    }
+
+    addCylinderVertical(material,x,y,z,base,height){
+        var geometry = new THREE.CylinderGeometry( base, base, height, 64 ); /*Verificar estes parametros*/
+        var mesh= new THREE.Mesh(geometry,material);
+        this.add(mesh);
+        mesh.rotateX(Math.PI/2);
+        mesh.position.set(x, y, z);
+    
+    }
+    
+    addCylinderHorizontal(x,y,z,base,height){
+    
+        var geometry = new THREE.CylinderGeometry( base, base, height, 64 ); /*Verificar estes parametros*/
+        var mesh= new THREE.Mesh(geometry,material);
+        this.add(mesh);
+        mesh.rotateX(Math.PI/2);
+        mesh.position.set(x, y, z);
+    }
+
+    addElemento(e,x,y,z){
+        this.add(e);
+        e.position.set(x,y,z);
+    }
+
+    changeWireframe() {
+        this.material.wireframe = !this.material.wireframe;
+    }
+
+}
---
+++ src_1/js/script.js
@@ -0,0 +1,217 @@
+/*global THREE*/
+
+var camera,FrontalCamera, TopCamera, LateralCamera;
+var scene, renderer;
+var mobile, branchOne, branchTwo, branchThree;
+
+//let distance = 5;
+
+/*function addRod(obj, x, y, z, h, a) {
+	'use strict';
+
+	let geometry = new THREE.CylinderGeometry(1, 1, h);
+	geometry.rotateZ(a * Math.PI / 180);
+	let material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+	let mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(x, y, z);
+	obj.add(mesh);
+
+}
+
+function createMobile(x, y, z) {
+	'use strict';
+	let mobile = new THREE.Object3D();
+
+	addRod(mobile, x, y, z, 5, 0);
+	addRod(mobile, x, y - 3.5, 0, 6, 90);
+
+	scene.add(mobile);
+
+}*/
+
+function createCamera(x,y,z) {
+	'use strict';
+	/*camera = new THREE.OrthographicCamera(-8 * distance, 8 * distance,
+		4.5 * distance,-4.5 * distance);
+	scene.add(camera);*/
+	var distance = 5;
+
+   	camera = new THREE.OrthographicCamera( window.innerWidth / -distance, 
+                                            window.innerWidth / distance, 
+                                            window.innerHeight / distance, 
+                                            window.innerHeight / -distance, 
+                                            1, 
+											1000);
+	camera.position.x = x;
+    camera.position.y = y;
+    camera.position.z = z;
+	camera.lookAt(scene.position);
+	return camera;
+
+}
+
+function createScene() {
+	'use strict';
+	scene = new THREE.Scene();
+
+	scene.add(new THREE.AxesHelper(20));
+	/* so para clarificar : 
+	eixo cor vermelha = eixo dos xx 
+	eixo cor azul = eixo dos yy
+	eixo cor verde = eixo dos zz 
+	
+	Nota: vi em outros projetos, quando
+	a camera está em (0,100,0) isto dá uma vista de cima em vez de lado como se fosse dos yy... 
+	 por tanto maybe depois alterar os nomes
+	das variaveis porque o Topcamera nao dá supostamente a vista de cima, mas sim o 3 é que da*/
+
+	//createMobile(0,0,0); /*Acho melhor criar uma classe chamada Mobile, dps faz-se new Mobile*/
+	mobile= new Mobile(0,0,0);
+	
+
+}
+
+function animate() {
+	'use strict';
+
+	/*funcoes de animacao sobre o mobile*/
+	var speed= 2;
+	// girar todo o mobile
+	if(mobile.getBranchOneRotation() != 0) {
+		if (mobile.branchOneRotation == 1)  //1 -> girar para um lado e 2-> girar para outro
+			mobile.rotateBranchOne(0);
+		else if(mobile.branchOneRotation == 2)
+			mobile.rotateBranchOne(-0);
+	}
+
+	//girar ramo2 
+	if(mobile.getBranchTwoRotation() != 0) {
+		if (mobile.branchTwoRotation == 1)  //1 -> girar para um lado e 2-> girar para outro
+			mobile.rotateBranchTwo(0);
+		else if(mobile.branchTwoRotation == 2)
+			mobile.rotateBranchTwo(-0);
+	}
+
+	//girar ramo3
+	if(mobile.getBranchThreeRotation() != 0) {
+		if (mobile.branchThreeRotation == 1)  //1 -> girar para um lado e 2-> girar para outro
+			mobile.rotateBranchThree(0);
+		else if(mobile.branchThreeRotation == 2)
+			mobile.rotateBranchThree(-0);
+			
+	}
+
+	//mover o mobile
+	if (robot.getMove()  != 0) {
+		if (robot.move == 1)
+			robot.moveRobot(speed);
+		else if(robot.move == 2)
+			robot.moveRobot(-speed);
+	}
+
+	requestAnimationFrame(animate);
+	render();
+	
+}
+
+
+
+function render() {
+	'use strict';
+	renderer.render(scene, camera);  
+}
+
+function __init__() {
+	'use strict';
+
+	renderer = new THREE.WebGLRenderer({antialias: true});
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	document.body.appendChild(renderer.domElement);
+
+	createScene();
+	FrontalCamera = createCamera(100,0,0);  //view from x
+	TopCamera= createCamera(0,0,100); //view from z
+	LateralCamera = createCamera(0,100,0); //view from y
+
+	render();
+	window.addEventListener("keydown",onKeyDown);
+}
+
+function onKeyDown(e) {   
+    switch (e.keyCode) {
+
+        case 49: //1
+			camera= FrontalCamera;
+            break;
+
+        case 50: //2
+			camera=TopCamera;
+            break;
+
+        case 51:  //3
+			camera=LateralCamera;
+            break;
+
+        case 52: //4
+			mobile.changeWireframe();
+	
+			break;
+			
+		case 87: // W
+		case 119: // w   /*Rodar v1 num sentido*/
+
+			mobile.rotateBranchOne(0); 
+			break;
+	
+		case 81: // Q  /*rodar v1 noutro sentido*/
+		case 113: //q
+			mobile.rotateBranchOne(-0);
+				
+		break;      
+	
+		case 65: // A
+        case 97: //a
+           	mobile.rotateBranchTwo(0);
+			break;
+			
+		case 68:  //D 
+		case 100: //d
+			mobile.rotateBranchTwo(-0);  
+		
+		case 90:  //Z 
+		case 122: //z
+			mobile.rotateBranchThree(0);
+			break;
+
+        case 83: // S
+        case 115: //s
+            mobile.rotateBranchThree(-0);
+			break;
+
+		case 38: // Seta para cima
+			mobile.setMobileMoving(1);
+
+        	break;
+
+        case 40: // Seta para baixo
+		   mobile.setMobileMoving(2);
+            break;
+
+        case 39: // Seta para o lado direito
+			mobile.setBaseMovement(1);
+            break;
+        
+        case 37: // Seta para o lado esquerdo
+			mobile.setBaseMovement(1);
+            break;
+
+       
+    }
+
+}
+
+
+
+
